2de0b1ac8d589e40fc086c97f2aa235e0c701054
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2015-07-06 15:50:35 +0800

    Fix Counting Inversion algorithm implementation

--- lib/counting_inversions.rb
+++ lib/counting_inversions.rb
@@ -13,7 +13,7 @@ def sort_count(array)
   sorted_right_half, y = sort_count(right)
   sorted_array, z = merge_and_count_split(sorted_left_half, sorted_right_half)
 
-  return array, (x + y + z)
+  return sorted_array, (x + y + z)
 end
 
 def merge_and_count_split(array1, array2)
@@ -22,15 +22,21 @@ def merge_and_count_split(array1, array2)
   i = 0
   j = 0
 
-  (array1.length + array2.length).times do |k|
-    if array1[i] && array1[i] <= array2[j]
-      result[k] = array1[i]
+  while i < array1.length && j < array2.length do
+    if array1[i] <= array2[j]
+      result << array1[i]
       i += 1
     else
-      result[k] = array2[j]
+      result << array2[j]
       inversions += array1.length - i
       j += 1
     end
+  end
+
+  if i < array1.length
+    result += array1[i..(array1.length - 1)]
+  else
+    result += array2[j..(array2.length - 1)]
   end
 
   return result, inversions
